e279cba0c05866e52c2d91a8180138234b9c9581
chenhan20232079 <1289052916@qq.com>
2025-11-01 21:30:38 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+﻿import "./assets/style.css"
+import { createApp } from "vue"
+import App from "./App.vue"
+import { inject } from "@vercel/analytics"
+
+// 注入Vercel Analytics
+inject()
+
+createApp(App).mount("#app")
